8ab787fce1d3e5a4b76560490cb1dcb78b93419b
Maria Kurinova <kma240190@mail.ru>
2022-04-03 13:50:23 +0300

    return

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -5,20 +5,13 @@ import getPath from '../src/getPath';
 const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8').trim();
 
 it('test genDiff format stylish', () => {
-  expect(genDiff('file1.json', 'file2.json', 'stylish')).toEqual(readFile('stylish.txt'));
-});
-it('test genDiff format stylish', () => {
   expect(genDiff('file1.yml', 'file2.yml', 'stylish')).toEqual(readFile('stylish.txt'));
 });
+
 it('test genDiff format plain', () => {
   expect(genDiff('file1.yml', 'file2.yml', 'plain')).toEqual(readFile('plain.txt'));
 });
-it('test genDiff format plain', () => {
-  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(readFile('plain.txt'));
-});
+
 it('test genDiff format json', () => {
   expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(readFile('json.txt'));
-});
-it('test genDiff format json', () => {
-  expect(genDiff('file1.yml', 'file2.yml', 'json')).toEqual(readFile('json.txt'));
 });
