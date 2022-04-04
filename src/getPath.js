724cd7cbe0d97629482e7b6ef7e6918dfbbbd8a8
Maria Kurinova <kma240190@mail.ru>
2022-04-04 19:39:24 +0300

    remove

--- src/getPath.js
+++
@@ -1,8 +0,0 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-export default getPath;
